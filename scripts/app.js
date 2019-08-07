9a82fcd5bb0e2d7c63f0bb9c9aea2f44ab0ae55e
Artem <gorbacartem90@gmail.com>
2019-08-07 23:35:11 +0300

    refactoring v1

--- scripts/app.js
+++ scripts/app.js
@@ -8,139 +8,96 @@ class Table {
   createTable() {
     /*......................Get function........................*/
 
-    const getTrArr = () => {
+    const getRows = () => {
       return table.querySelectorAll("tr");
-    };
-
-    const getPositionElement = target => {
-      return target.getBoundingClientRect();
-    };
-
-    const getNumberColumn = () => {
-      return getTrArr()[0].querySelectorAll("td").length;
     };
 
     /*......................Create tag function........................*/
 
-    const createTrElement = () => {
-      let tr = document.createElement("tr");
-      tr.classList.add("table-row");
-      return tr;
+    const createElement = ({ type, classList, innerText }) => {
+      const element = document.createElement(type);
+      element.classList.add(...classList);
+      if (innerText) {
+        element.innerText = innerText;
+      }
+      return element;
     };
 
-    const createTdElement = () => {
-      let td = document.createElement("td");
-      td.classList.add("cell");
-      return td;
-    };
-
-    const table = document.createElement("table");
+    const createRow = () =>
+      createElement({ type: "tr", classList: ["table-row"] });
+    const createCell = () => createElement({ type: "td", classList: ["cell"] });
+    const table = createElement({ type: "table", classList: ["table"] });
 
     /*......................Work with rows and columns........................*/
 
+    const findCell = target => {
+      const element = target.getBoundingClientRect();
+      return document.elementFromPoint(
+        target.classList.contains("btn-remove-row")
+          ? element.width + element.right
+          : element.right,
+        target.classList.contains("btn-remove-column")
+          ? element.height + element.bottom
+          : element.bottom
+      );
+    };
+
     const addRow = () => {
-      let tr = createTrElement();
+      const row = createRow();
       for (let i = 0; i < this.column; i++) {
-        let td = createTdElement();
-        tr.appendChild(td);
+        row.appendChild(createCell());
       }
-      table.appendChild(tr);
+      table.appendChild(row);
       this.row++;
     };
 
     const addColumn = () => {
+      const rows = getRows();
       for (let i = 0; i < this.row; i++) {
-        let td = createTdElement();
-        let tr = getTrArr()[i];
-        tr.appendChild(td);
+        rows[i].appendChild(createCell());
       }
       this.column++;
     };
 
     const removeRow = e => {
-      const target = e.target;
-      const positionElement = getPositionElement.bind(this, target);
-      const findTd = () =>
-        document.elementFromPoint(
-          positionElement().right + positionElement().width,
-          positionElement().bottom
-        );
-      const findTr = () => findTd().parentNode;
-      const td = findTd();
-      if (td) {
-        if (td.classList.contains("cell")) {
-          const tr = findTr();
-          table.removeChild(tr);
-          this.row--;
-          /*
-          move the button if it is not opposite the cell
-          */
-          if (!findTr().classList.contains("table-row")) {
-            const lastElem = table.lastChild;
-            btnRemoveRow.style.top = lastElem.offsetTop.toString() + "px";
-          }
-          if (getTrArr().length === 1) {
-            btnRemoveRow.classList.remove("btn-remove-visible");
-          }
-        }
+      const currentCell = findCell(e.target);
+      if (!currentCell || !currentCell.classList.contains("cell")) {
+        return;
+      }
+      table.removeChild(currentCell.parentNode);
+      this.row--;
+      if (!findCell(e.target).classList.contains("cell")) {
+        btnRemoveRow.style.top = table.lastChild.offsetTop + "px";
+      }
+      if (getRows().length === 1) {
+        btnRemoveRow.classList.remove("btn-show");
       }
     };
 
     const removeColumn = e => {
-      const target = e.target;
-      const positionElement = getPositionElement.bind(this, target);
-      const findTdFun = () =>
-        document.elementFromPoint(
-          positionElement().right,
-          positionElement().bottom + positionElement().height
-        );
-      const td = findTdFun();
-      if (td) {
-        if (td.classList.contains("cell")) {
-          const tdNum = [...td.parentElement.children].indexOf(td);
-          const allTr = getTrArr();
-          for (let i = 0; i < this.row; i++) {
-            let removeTd = allTr[i].children[tdNum];
-            allTr[i].removeChild(removeTd);
-          }
-          this.column--;
-          if (!findTdFun().classList.contains("cell")) {
-            const lastElem = getTrArr()[0].lastChild;
-            btnRemoveColumn.style.left = lastElem.offsetLeft.toString() + "px";
-          }
-          if (getNumberColumn() === 1) {
-            btnRemoveColumn.classList.remove("btn-remove-visible");
-          }
-        }
+      const currentCell = findCell(e.target);
+      if (!currentCell || !currentCell.classList.contains("cell")) {
+        return;
+      }
+      const indexCell = Array.from(currentCell.parentNode.children).indexOf(
+        currentCell
+      );
+      const rows = getRows();
+      for (let i = 0; i < this.row; i++) {
+        rows[i].removeChild(rows[i].children[indexCell]);
+      }
+      this.column--;
+      if (!findCell(e.target).classList.contains("cell")) {
+        btnRemoveColumn.style.left = getRows()[0].lastChild.offsetLeft + "px";
+      }
+      if (this.column === 1) {
+        btnRemoveColumn.classList.remove("btn-show");
       }
     };
 
     /*......................Button functions........................*/
 
-    const createBtn = () => {
-      const btn = document.createElement("button");
-      btn.classList.add("btn");
-      return btn;
-    };
-
-    const createAddBtn = () => {
-      const addBtn = createBtn();
-      addBtn.classList.add("btn-add");
-      addBtn.innerText = "+";
-      return addBtn;
-    };
-
-    const createRemoveBtn = () => {
-      const removeBtn = createBtn();
-      removeBtn.classList.add("btn-remove");
-      removeBtn.innerText = "-";
-      return removeBtn;
-    };
-
-    const moveRemoveBtn = (btnRemoveColumn, btnRemoveRow, e) => {
-      /*
-      move the delete buttons on the x and y axis
-      */
+    const moveButton = e => {
       const target = e.target;
       if (target.classList.contains("cell")) {
         btnRemoveColumn.style.left = target.offsetLeft.toString() + "px";
@@ -148,129 +105,76 @@ class Table {
       }
     };
 
-    const removeBtnVisible = () => {
-      /*
-      display only if the number of columns> 1;
-      */
-      if (getNumberColumn() > 1) {
-        btnRemoveColumn.classList.add("btn-remove-visible");
+    const showButton = () => {
+      if (this.column > 1) {
+        btnRemoveColumn.classList.add("btn-show");
       }
-      /*
-      display only if the number of lines> 1;
-      */
-      if (getTrArr().length > 1) {
-        btnRemoveRow.classList.add("btn-remove-visible");
+      if (this.row > 1) {
+        btnRemoveRow.classList.add("btn-show");
       }
     };
 
-    const removeBtnHidden = (...arg) => {
-      try {
-        for (let i in arg) {
-          arg[i].classList.remove("btn-remove-visible");
-        }
-      } catch (e) {}
-    };
-
-    /*......................Listeners........................*/
-
-    const moveRemoveBtnListener = (elem, btnRemoveColumn, btnRemoveRow) => {
-      const moveBtn = moveRemoveBtn.bind(this, btnRemoveColumn, btnRemoveRow);
-      elem.addEventListener("mouseover", moveBtn);
-    };
-
-    const addRowListener = btn => {
-      btn.addEventListener("click", addRow);
-    };
-
-    const addColumnListener = btn => {
-      btn.addEventListener("click", addColumn);
-    };
-
-    const removeRowListener = btn => {
-      const removeRowFunction = removeRow.bind(this);
-      btn.addEventListener("click", removeRowFunction);
-    };
-
-    const removeColumnListener = btn => {
-      const removeColumnFunction = removeColumn.bind(this);
-      btn.addEventListener("click", removeColumnFunction);
-    };
-
-    const removeBtnVisibleListener = elem => {
-      elem.addEventListener("mouseover", removeBtnVisible);
-    };
-
-    const removeBtnHiddenListener = (elem, ...arg) => {
-      const btnHidden = removeBtnHidden.bind(this, ...arg);
-      elem.addEventListener("mouseout", btnHidden);
-    };
-
-    const removeBtnIsActiveListener = btn => {
-      btn.addEventListener("mouseover", removeBtnVisible);
-    };
-
-    const removeBtnIsInActiveListener = (btn1, btn2) => {
-      const btnHidden = removeBtnHidden.bind(this, btn1, btn2);
-      btn1.addEventListener("mouseout", btnHidden);
-      btn2.addEventListener("mouseout", btnHidden);
+    const hiddenButton = () => {
+      btnRemoveColumn.classList.remove("btn-show");
+      btnRemoveRow.classList.remove("btn-show");
     };
 
     /*......................Table creation........................*/
 
-    /*
-    to prevent the reuse of the creative function in an instance
-    */
     if (this.flag) {
       return console.log("table already created!!!");
     }
     this.flag = true;
-
     if (!this.row || !this.column) {
       return null;
     }
 
-    const div = document.createElement("div");
-    const btnAddColumn = createAddBtn();
-    const btnAddRow = createAddBtn();
-    const btnRemoveColumn = createRemoveBtn();
-    const btnRemoveRow = createRemoveBtn();
-    btnAddColumn.classList.add("btn-add-column");
-    btnAddRow.classList.add("btn-add-row");
-    btnRemoveColumn.classList.add("btn-remove-column");
-    btnRemoveRow.classList.add("btn-remove-row");
+    const div = createElement({ type: "div", classList: ["content"] });
+    const btnAddColumn = createElement({
+      type: "button",
+      innerText: "+",
+      classList: ["btn", "btn-add", "btn-add-column"]
+    });
+    const btnAddRow = createElement({
+      type: "button",
+      innerText: "+",
+      classList: ["btn", "btn-add", "btn-add-row"]
+    });
+    const btnRemoveColumn = createElement({
+      type: "button",
+      innerText: "-",
+      classList: ["btn", "btn-remove", "btn-remove-column"]
+    });
+    const btnRemoveRow = createElement({
+      type: "button",
+      innerText: "-",
+      classList: ["btn", "btn-remove", "btn-remove-row"]
+    });
 
     for (let j = 0; j < this.row; j++) {
-      let tr = createTrElement();
+      let tr = createRow();
       for (let i = 0; i < this.column; i++) {
-        let td = createTdElement();
-        tr.appendChild(td);
+        tr.appendChild(createCell());
       }
       table.appendChild(tr);
     }
-
-    table.classList.add("table");
-    div.appendChild(table);
-    div.classList.add("content");
     document.body.appendChild(div);
+    div.appendChild(table);
     div.appendChild(btnAddColumn);
     div.appendChild(btnAddRow);
     div.appendChild(btnRemoveColumn);
     div.appendChild(btnRemoveRow);
-    /*
-    in the code below is the addition of listeners
-    */
-    addRowListener(btnAddRow);
-    addColumnListener(btnAddColumn);
-    removeRowListener(btnRemoveRow);
-    removeColumnListener(btnRemoveColumn);
-    moveRemoveBtnListener(table, btnRemoveColumn, btnRemoveRow);
-    removeBtnVisibleListener(table);
-    removeBtnHiddenListener(table, btnRemoveColumn, btnRemoveRow);
-    removeBtnIsActiveListener(btnRemoveRow);
-    removeBtnIsActiveListener(btnRemoveColumn);
-    removeBtnIsInActiveListener(btnRemoveColumn, btnRemoveRow);
-    removeBtnHiddenListener(btnRemoveColumn, btnRemoveColumn, btnRemoveRow);
-    removeBtnHiddenListener(btnRemoveRow, btnRemoveColumn, btnRemoveRow);
+    btnAddRow.addEventListener("click", addRow);
+    btnAddColumn.addEventListener("click", addColumn);
+    btnRemoveRow.addEventListener("click", removeRow);
+    btnRemoveRow.addEventListener("mouseover", showButton);
+    btnRemoveRow.addEventListener("mouseout", hiddenButton);
+    btnRemoveColumn.addEventListener("click", removeColumn);
+    btnRemoveColumn.addEventListener("mouseover", showButton);
+    btnRemoveColumn.addEventListener("mouseout", hiddenButton);
+    table.addEventListener("mouseover", moveButton);
+    table.addEventListener("mouseover", showButton);
+    table.addEventListener("mouseout", hiddenButton);
   }
 }
 
